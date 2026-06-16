6274216ab3542f7567efc141b3f0e2ee366db563
Alok Singh <alok.singh@nuvo.ai>
2026-06-16 18:52:47 +0530

    lru cache

--- lru.cpp
+++ lru.cpp
@@ -3,64 +3,67 @@ using namespace std;
 
 class LRUCache {
 public:
-  class Node {
-  public:
-    int key;
-    int value;
-    Node *next;
-    Node *prev;
-    Node(int key, int value) {
-      this->key = key;
-      this->value = value;
+    class Node{
+        public:
+            int key;
+            int value;
+            Node* next;
+            Node* prev;
+            Node(int key, int value){
+                this->key = key;
+                this->value = value;
+                next = nullptr;
+                prev= nullptr;
+            }
+    };
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
+    int cap;
+    unordered_map<int, Node*> mp;
+    void addNode(Node* newNode){
+        Node* temp= head->next;
+        head->next = newNode;
+        newNode->next = temp;
+        temp->prev= newNode;
+        newNode->prev= head;
     }
-  };
-
-  Node *head = new Node(-1, -1);
-  Node *tail = new Node(-1, -1);
-  int cap;
-  unordered_map<int, Node *> mp;
-
-  LRUCache(int capacity) {
-    cap = capacity;
-    head->next = tail;
-    tail->prev = head;
-  }
-  void addNode(Node *newNode) {
-    Node *temp = head->next;
-    newNode->next = temp;
-    temp->prev = newNode;
-    head->next = newNode;
-    newNode->prev = head;
-  }
-  void deleteNode(Node *delNode) {
-    Node *delPrev = delNode->prev;
-    Node *delNext = delNode->next;
-    delPrev->next = delNext;
-    delNext->prev = delPrev;
-  }
-  int get(int key) {
-    if (mp.find(key) != mp.end()) {
-      Node *resNode = mp[key];
-      int res = resNode->value;
-      deleteNode(resNode);
-      addNode(resNode);
-      mp[key] = head->next;
-      return res;
+    void deleteNode(Node* delNode){
+        Node* nextNode= delNode->next;
+        Node* prevNode= delNode->prev;
+        nextNode->prev = prevNode;
+        prevNode->next = nextNode;
     }
-    return -1;
-  }
-
-  void put(int key, int value) {
-    if (mp.find(key) != mp.end()) {
-      Node *existingNode = mp[key];
-      mp.erase(key);
-      deleteNode(existingNode);
+    LRUCache(int capacity) {
+        cap = capacity;
+        head->next = tail;
+        tail->prev=head;
     }
-    if (mp.size() >= cap) {
-      mp.erase(tail->prev->key);
-      deleteNode(tail->prev);
+    
+    int get(int key) {
+        if(mp.find(key)!= mp.end()){
+            int res = mp[key]->value;
+            Node* resNode = mp[key];
+            deleteNode(resNode);
+            addNode(resNode);
+            return res;
+        }
+        return -1;
     }
-    addNode(new Node(key, value));
-    mp[key] = head->next;
-  }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){
+            Node* existingNode = mp[key];
+            mp.erase(key);
+            deleteNode(existingNode);
+            delete existingNode;
+        }
+        if(mp.size()>=cap){
+            Node* lru = tail->prev;
+            mp.erase(lru->key);
+            deleteNode(lru);
+            delete lru;
+        }
+        addNode(new Node(key, value));
+        mp[key]=head->next;
+    }
 };
