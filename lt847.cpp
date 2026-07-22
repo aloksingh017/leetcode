3cae1a82018812042882b6d3353a5d21f482e022
Alok Singh <alok.singh@nuvo.ai>
2026-07-22 11:50:08 +0530

    leetcode 847

--- lt847.cpp
+++ lt847.cpp
@@ -1,3 +1,51 @@
+/*
+847. Shortest Path Visiting All Nodes
+
+You have an undirected, connected graph of n nodes labeled from 0 to n - 1. You are given an array graph where graph[i] is a list of all the nodes connected with node i by an edge.
+
+Return the length of the shortest path that visits every node. You may start and stop at any node, you may revisit nodes multiple times, and you may reuse edges.
+
+Example 1:
+Input: graph = [[1,2,3],[0],[0],[0]]
+Output: 4
+Explanation: One possible path is [1,0,2,0,3]
+
+Example 2:
+Input: graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+Output: 4
+Explanation: One possible path is [0,1,4,2,3]
+ 
+
+Constraints:
+
+n == graph.length
+1 <= n <= 12
+0 <= graph[i].length < n
+graph[i] does not contain i.
+If graph[a] contains b, then graph[b] contains a.
+The input graph is always connected.
+ 
+Time Complexity
+
+The BFS state is represented by:
+
+(node, mask)
+node can take n values.
+mask can take 2ⁿ values.
+
+Hence, the total number of possible states is n×2^n
+
+Each state is processed at most once because of the visited array. For every processed state, you iterate through all neighbors of node.
+
+Across all states, each edge can be explored for every possible mask.
+
+Therefore, O((n+m)⋅2^n)
+
+Space
+O(n.2^n))
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
